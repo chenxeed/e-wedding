dcaa726d38221e11b54c290593b4a6ce021e49dc
Albert Mulia Shintra <chenxeed@gmail.com>
2021-05-31 00:22:26 +0800

    feat: init home & nav

---
+++ src/components/home.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+import { slide } from 'svelte/transition';
+import confetti from 'canvas-confetti';
+
+function showConfetti (event: Event) {
+  const element = event.target as HTMLElement
+  const bound = element.getBoundingClientRect()
+  const x = ((bound.left + bound.right) / 2) / window.innerWidth
+  const y = ((bound.top + bound.bottom) / 2) / window.innerHeight
+  confetti({
+    origin: {
+      x,
+      y
+    }
+  })
+}
+
+</script>
+<div class="flex flex-col items-center justify-evenly h-full">
+  <p
+    class="text-lg font-semibold text-center"
+    transition:slide="{{ duration: 2000, delay: 1000 }}"
+    on:introend={e => showConfetti(e)}>You are cordially invited to attend the wedding of</p>
+  <p
+    class="text-3xl font-extrabold text-center"
+    transition:slide="{{ duration: 2000, delay: 3000 }}"
+    on:introend={e => showConfetti(e)}>
+      Kelvin
+      <br>&
+      <br>Rita
+  </p>
+  <p
+    class="text-xl font-extrabold text-center"
+    transition:slide="{{ duration: 2000, delay: 5000 }}"
+    on:introend={e => showConfetti(e)}>Sunday, 12th Sept '21<br/>7 - 9PM
+  </p>
+  <p
+    class="text-xl font-extrabold text-center"
+    transition:slide="{{ duration: 2000, delay: 7000 }}"
+    on:introend={e => showConfetti(e)}>Puri Tower 12th Floor<br/>
+    <span class="text-lg">Jl. Martha Tilaar Block 69, Jakarta Pusat</span>
+  </p>
+</div>
+<style lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+
+  </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,46 @@
 <script lang="ts">
-  import Entrance from '../components/entrance.svelte'
+import { fade } from 'svelte/transition'
+import Entrance from '../components/entrance.svelte'
+import Home from '../components/home.svelte'
 
-  function onEntranceDone () {
-    alert('lets go')
-  }
+let showEntrance = true
+
 </script>
 <div>
-  <Entrance
-    on:done={onEntranceDone}/>
+  {#if showEntrance}
+    <section>
+      <Entrance on:done={() => showEntrance = false}/>
+    </section>
+  {:else}
+    <div in:fade>
+      <div class="flex flex-col h-screen">
+        <main class="flex-grow overflow-auto h-full">
+          <Home/>
+        </main>
+        <nav class="flex justify-around border-t-2 border-gray-700 py-3">
+          <div class="flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+            </svg>
+            Home
+          </div>
+          <div class="flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+            </svg>
+            Our Story
+          </div>
+          <div class="flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+              <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+            </svg>
+            Join Us
+          </div>
+        </nav>
+      </div>
+    </div>
+  {/if}
 </div>
 <style global lang="postcss">
 @tailwind base;
