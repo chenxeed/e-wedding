e5deb3ce0a1a76d5b7ed93d3de07c1e1babb9b60
Albert Mulia Shintra <chenxeed@gmail.com>
2021-06-12 15:37:40 +0800

    build: ready for gh-pages release

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,22 @@ import adapter from '@sveltejs/adapter-static';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
+const commandArgs = process.argv.reduce((arg, val) => {
+	const splitVal = val.split("=")
+	arg[splitVal[0]] = splitVal[1]
+	return arg
+}, {})
+
+// Define paths
+const paths = commandArgs['paths']
+? {
+	paths: {
+		base: commandArgs['paths'],
+		assets: commandArgs['paths']
+	}
+}
+: {}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -19,6 +35,8 @@ const config = {
 	],
 
 	kit: {
+		appDir: 'app',
+		...paths,
 		adapter: adapter({
 			// default options are shown
 			pages: 'build',
