6c430dd55ce23aecfef32b80542366f24677f1b9
Albert Mulia Shintra <chenxeed@gmail.com>
2021-06-23 01:52:45 +0800

    feat: navigation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,6 +7,15 @@ import Join from '../components/join.svelte'
 
 let showEntrance = true
 
+function scrollTo (targetId: string) {
+  const target = document.getElementById(targetId)
+  if (target) {
+    target.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+}
+
 </script>
 <div>
   {#if showEntrance}
@@ -18,31 +27,37 @@ let showEntrance = true
       <div class="h-screen">
         <main class="overflow-auto page-height">
           <div class="bg-gradient-to-b from-red-100 to-red-400 divide-y-8 divide-pink-200">
-            <Home/>
-            <Story/>
-            <Join/>
+            <div id="home">
+              <Home/>
+            </div>
+            <div id="story">
+              <Story/>
+            </div>
+            <div id="join">
+              <Join/>
+            </div>
           </div>
         </main>
         <nav class="flex-grow-0 flex justify-around items-center border-t-2 border-gray-700">
-          <div class="flex flex-col items-center">
+          <a class="flex flex-col items-center" href="#home" on:click|preventDefault={() => scrollTo('home')}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
             </svg>
             Home
-          </div>
-          <div class="flex flex-col items-center">
+          </a>
+          <a class="flex flex-col items-center" href="#story" on:click|preventDefault={() => scrollTo('story')}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
             </svg>
             Our Story
-          </div>
-          <div class="flex flex-col items-center">
+          </a>
+          <a class="flex flex-col items-center" href="#join" on:click|preventDefault={() => scrollTo('join')}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
               <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
             </svg>
             Join Us
-          </div>
+          </a>
         </nav>
       </div>
     </div>
@@ -77,6 +92,10 @@ let showEntrance = true
       text-shadow: none;
     }
   }
+}
+
+html {
+  scroll-behavior: smooth;
 }
 
 nav {
