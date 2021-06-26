85eb8b66644d29ab4ebe379747a4204c74476084
Albert Mulia Shintra <chenxeed@gmail.com>
2021-06-26 19:33:03 +0800

    fix: nav bg missing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,7 +38,7 @@ function scrollTo (targetId: string) {
             </div>
           </div>
         </main>
-        <nav class="flex-grow-0 flex justify-around items-center border-t-2 border-gray-700 fixed bottom-0 w-full">
+        <nav class="flex-grow-0 flex justify-around items-center border-t-2 border-gray-700 fixed bottom-0 w-full bg-red-50">
           <a class="flex flex-col items-center" href="#home" on:click|preventDefault={() => scrollTo('home')}>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
